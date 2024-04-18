19a8db146bdc1ae26713d5da915498d2a1bfd9c3
jakubkrzywda <qbakrzywda@gmail.com>
2024-04-18 20:15:54 +0200

    Added script file and made a mess

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Users
+from .serializers import UserSerializer
+
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializer
